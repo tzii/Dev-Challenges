4c157d5ede651f13d4b42fb3e41e063ee66966ba
Tuấn Vũ <tuanvundvn@gmail.com>
2020-09-13 17:56:34 +0700

    change button component

---
+++ Button Component/precache-manifest.0d1494cd604fc72f90513716a7fbb0e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4762523ece4aa5284a7d95d5a7087e2f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "dd5fd9a359341515d3dd",
+    "url": "./static/css/main.24ac9a1b.chunk.css"
+  },
+  {
+    "revision": "4de0d3673e85cf27f896",
+    "url": "./static/js/2.87ecdd85.chunk.js"
+  },
+  {
+    "revision": "ca962714ee3537eb863ad2954c33c63e",
+    "url": "./static/js/2.87ecdd85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd5fd9a359341515d3dd",
+    "url": "./static/js/main.85f060a7.chunk.js"
+  },
+  {
+    "revision": "9eee317e2dfb406391e0",
+    "url": "./static/js/runtime-main.4e11e643.js"
+  }
+]);
